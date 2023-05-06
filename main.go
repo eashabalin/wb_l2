b7b5535c9866248c28605cc5b466f6b07cbf1e48
Egor Shabalin <eashabalin.1@gmail.com>
2023-05-06 14:25:09 +0300

    dev05 updated

--- main.go
+++ main.go
@@ -1,9 +1,39 @@
 package main
 
-import (
-	"wb_l2/develop/dev05"
-)
+import "fmt"
+
+func merge(arrs ...[]int) []int {
+	length := 0
+	for _, a := range arrs {
+		length += len(a)
+	}
+	res := make([]int, length)
+	h := res[0:length]
+	for _, arr := range arrs {
+		copy(h, arr)
+		h = h[len(arr):]
+	}
+	return res
+}
+
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	a := 0
+	b := 1
+	c := 1
+	for i := 2; i <= n; i++ {
+		c = a + b
+		a = b
+		b = c
+	}
+	return c
+}
 
 func main() {
-	dev05.Main()
+	fmt.Println(fib(8))
 }
